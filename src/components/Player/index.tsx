681589921fc1a94466e6ea627ae97ead4b275dad
DerRafDev <rafael.sordi.lino@gmail.com>
2021-04-25 16:28:12 -0400

    Add Audio

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { useContext } from "react";
+import { useContext, useRef, useEffect } from "react";
 import Slider from 'rc-slider';
 
 import 'rc-slider/assets/index.css';
@@ -9,7 +9,28 @@ import { PlayerContext } from "../../contexts/PlayerContext";
 import styles from "./styles.module.scss";
 
 export function Player() {
-    const { episodeList, currentEpisodeIndex } = useContext(PlayerContext)
+    const audioRef = useRef<HTMLAudioElement>(null);
+
+    const { 
+        episodeList, 
+        currentEpisodeIndex, 
+        isPlaying, 
+        togglePlay,
+        setPlayingState
+    } = useContext(PlayerContext)
+
+    //this is for when isPlaying is used, to the buttons work
+    useEffect(() => {
+        if (!audioRef.current){
+            return;
+        }
+
+        if(isPlaying) {
+            audioRef.current.play();
+        } else {
+            audioRef.current.pause();
+        }
+    }, [isPlaying])
 
     const episode = episodeList[currentEpisodeIndex]
 
@@ -53,6 +74,17 @@ export function Player() {
                     </div>
                     <span>00:00</span>
                 </div>
+                
+                
+                { episode && (
+                    <audio
+                        src={episode.url}
+                        ref={audioRef}
+                        autoPlay
+                        onPlay={() => setPlayingState(true)}
+                        onPause={() => setPlayingState(false)}
+                    />
+                )}
 
                 <div className={styles.buttons}>
                     <button type="button" disabled={!episode}>
@@ -61,8 +93,15 @@ export function Player() {
                     <button type="button" disabled={!episode}>
                         <img src="/play-previous.svg" alt="Play previous"/>
                     </button>
-                    <button type="button" className={styles.playButton} disabled={!episode}>
-                        <img src="/play.svg" alt="Play"/>
+                    <button 
+                        type="button"
+                        className={styles.playButton}
+                        disabled={!episode}
+                        onClick={togglePlay}
+                    >
+                        { isPlaying
+                        ? <img src="/pause.svg" alt="Play"/>
+                        : <img src="/play.svg" alt="Play"/> }
                     </button>
                     <button type="button" disabled={!episode}>
                         <img src="/play-next.svg" alt="Play next"/>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,15 +10,27 @@ import { useState } from 'react';
 function MyApp({ Component, pageProps }) {
   const [episodeList, setEpisodeList] = useState([]);
   const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   //this is for the button play to work, in a way that will put in the episodeList
   function play(episode) {
     setEpisodeList([episode])
     setCurrentEpisodeIndex(0);
+    setIsPlaying(true);
+  }
+
+  //this is for play/pause the button
+  function togglePlay() {
+    setIsPlaying(!isPlaying);
+  }
+
+  //this is for the play/pause work even in your keyboard
+  function setPlayingState(state: boolean) {
+    setIsPlaying(state);
   }
 
   return (
-    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play }}>
+    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play, isPlaying, togglePlay, setPlayingState }}>
       <div className={styles.wrapper}>
         <main>
           <Header />
